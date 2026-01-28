2f51ae9cd4e06639f1d9639b0a71c9030ce58574
Jerome Riga <jriga@zemis.co.uk>
2026-01-28 16:48:56 +0000

    adjust ineligible_for_funding
    Following changes to fundingeligibility

--- app/forms/questionnaires/ineligible_for_funding.rb
+++ app/forms/questionnaires/ineligible_for_funding.rb
@@ -6,13 +6,7 @@ module Questionnaires
 
     NOT_ELIGIBLE_FOR_SCHOLARSHIP_FUNDING = "not_eligible_for_scholarship_funding".freeze
     NOT_IN_ENGLAND = "not_in_england".freeze
-    EARLY_YEARS_INELIGIBLE_ESTABLISHMENT = "early_years/outside_catchment_or_not_on_early_years_register".freeze
-    EARLY_YEARS_NOT_APPLYING_FOR_NPQEY = "early_years/not_applying_for_NPQEY".freeze
-    LEAD_MENTOR_NOT_APPLYING_FOR_NPQLTD = "lead_mentor/not_applying_for_NPQLTD".freeze
-
-    # Already funded
     ALREADY_FUNDED_NOT_ELIGIBLE_SCHOLARSHIP_FUNDING = "already_funded/not_eligible_scholarship_funding".freeze
-    ALREADY_FUNDED_NOT_ELIGIBLE_SCHOLARSHIP_FUNDING_NOT_TSF = "already_funded/not_eligible_scholarship_funding_not_tsf".freeze
 
     attr_accessor :version
 
@@ -21,41 +15,20 @@ module Questionnaires
     end
 
     def previous_step
-      # TODO: match each previous step according to question TTE
       :teacher_catchment
     end
 
     def ineligible_template
       @ineligible_template ||= case funding_eligiblity_status_code
                                when FundingEligibility::NOT_IN_ENGLAND
-                                 return NOT_IN_ENGLAND
-                               when FundingEligibility::NOT_LEAD_MENTOR_COURSE
-                                 return LEAD_MENTOR_NOT_APPLYING_FOR_NPQLTD
-                               when FundingEligibility::INELIGIBLE_ESTABLISHMENT_TYPE
-                                 return NOT_ELIGIBLE_FOR_SCHOLARSHIP_FUNDING
+                                 NOT_IN_ENGLAND
                                when FundingEligibility::PREVIOUSLY_FUNDED
-                                 if tsf_elgible?
-                                   return ALREADY_FUNDED_NOT_ELIGIBLE_SCHOLARSHIP_FUNDING
-                                 else
-                                   return ALREADY_FUNDED_NOT_ELIGIBLE_SCHOLARSHIP_FUNDING_NOT_TSF
-                                 end
-                               when FundingEligibility::NOT_ON_EARLY_YEARS_REGISTER
-                                 return EARLY_YEARS_INELIGIBLE_ESTABLISHMENT
-                               when FundingEligibility::EARLY_YEARS_INVALID_NPQ
-                                 return EARLY_YEARS_NOT_APPLYING_FOR_NPQEY
-                               when FundingEligibility::NOT_ENTITLED_EY_INSTITUTION
-                                 return "not_entitled_ey_institution"
-                               when FundingEligibility::INELIGIBLE_ESTABLISHMENT_NOT_A_PP50
-                                 return "not_a_pp50_institution"
-                               when FundingEligibility::NOT_ENTITLED_CHILDMINDER
-                                 return "not_entitled_ey_institution"
-                               when FundingEligibility::NO_INSTITUTION
-                                 return NOT_ELIGIBLE_FOR_SCHOLARSHIP_FUNDING
-                               when FundingEligibility::INELIGIBLE_INSTITUTION_TYPE
-                                 return NOT_ELIGIBLE_FOR_SCHOLARSHIP_FUNDING
+                                 ALREADY_FUNDED_NOT_ELIGIBLE_SCHOLARSHIP_FUNDING
+                               when FundingEligibility::INELIGIBLE_SETTING
+                                 NOT_ELIGIBLE_FOR_SCHOLARSHIP_FUNDING
+                               else
+                                 raise UnexpectedEligibilityStatusCode, "Missing status code handling: #{funding_eligiblity_status_code}"
                                end
-
-      raise UnexpectedEligibilityStatusCode, "Missing status code handling: #{funding_eligiblity_status_code}"
     end
 
     def funding_eligiblity_status_code
@@ -66,35 +39,16 @@ module Questionnaires
       @funding_eligibility ||= FundingEligibility.new(
         course:,
         institution:,
-        approved_itt_provider: approved_itt_provider?,
         inside_catchment: inside_catchment?,
-        new_headteacher: new_headteacher?,
         trn: wizard.query_store.trn,
         get_an_identity_id: wizard.query_store.get_an_identity_id,
-        lead_mentor_for_accredited_itt_provider: lead_mentor_for_accredited_itt_provider?,
         query_store: wizard.query_store,
       )
     end
 
-    def tsf_elgible?
-      targeted_delivery_funding_eligibility? || tsf_primary_eligibility? || tsf_primary_plus_eligibility?
-    end
-
-    def funding_amount
-      @funding_amount ||= targeted_delivery_funding_eligibility? && tsf_primary_plus_eligibility? ? 800 : 200
-    end
-
     delegate :course,
-             :targeted_delivery_funding_eligibility?,
-             :tsf_primary_plus_eligibility?,
-             :tsf_primary_eligibility?,
              :lead_provider,
-             :new_headteacher?,
              :inside_catchment?,
-             :approved_itt_provider?,
-             :lead_mentor_for_accredited_itt_provider?,
-             :works_in_another_setting?,
-             :employment_type_other?,
              to: :query_store
   end
 end
