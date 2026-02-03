d0178c441e1d45736d2d043c712b06cdb643b0a9
Toby Privett <toby@snaplab.co.uk>
2026-02-03 11:29:59 +0000

    Add choose a TTE and provider screen

---
+++ spec/features/journeys/sad_paths/choose_a_tte_and_provider_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.feature "Choose a TTE and provider", :with_default_schedules, type: :feature do
+  include Helpers::JourneyAssertionHelper
+  include Helpers::JourneyStepHelper
+
+  let(:user) { create(:user) }
+  let(:course) { Course.find_by(identifier: "tte-early-years") }
+
+  before do
+    page.set_rack_session(
+      "user_id" => user.id,
+      "registration_store" => {
+        "course_start_date" => "yes",
+        "course_identifier" => course.identifier,
+        "lead_provider_id" => nil,
+      },
+    )
+  end
+
+  scenario "displays the page with correct content" do
+    visit "/registration/choose-a-tte-and-provider"
+
+    expect(page).to have_content("Choose a TTE course and provider")
+  end
+
+  scenario "back link returns to choose your provider page" do
+    visit "/registration/choose-a-tte-and-provider"
+
+    click_link "Back"
+
+    expect(page).to have_current_path("/registration/choose-your-provider")
+  end
+end
