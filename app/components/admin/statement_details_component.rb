c549aeaa6883275e535a36b96609ba84b0242361
Jerome Riga <jriga@zemis.co.uk>
2026-02-03 18:56:43 +0000

    Remove NpqSeparation namespace from components

---
+++ app/components/admin/statement_details_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Admin
+  class StatementDetailsComponent < BaseComponent
+    attr_reader :calculator, :link_to_voids, :statement
+
+    def initialize(statement:, link_to_voids: true)
+      @calculator = ::Statements::SummaryCalculator.new(statement:)
+      @link_to_voids = link_to_voids
+      @statement = statement
+    end
+  end
+end
