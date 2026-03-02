09f1a6ce46de4ceabcbe419feb01f6f6732f52b4
Ben Keeping <benjaminkeeping@gmail.com>
2026-03-02 14:47:42 +0000

    cascade delete on cohort/course_cohort

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -4,6 +4,7 @@ class Cohort < ApplicationRecord
   has_many :statements, dependent: :restrict_with_exception
   has_many :delivery_partnerships, dependent: :destroy
   has_many :delivery_partners, through: :delivery_partnerships
+  has_many :course_cohorts, dependent: :destroy
 
   validates :start_year,
             presence: true,
