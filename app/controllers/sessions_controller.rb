d31c557b46d71bd61d4d2cc11a913ce8c98a06a8
Toby Privett <toby@snaplab.co.uk>
2026-03-05 14:13:43 +0000

    Implement Sign Out for Teacher Auth
    - Store id_token during login for logout
    - Pass id_token_hint to auth provider on sign out
    - Route admin sign out to /admin
    
    https://github.com/DFE-Digital/teacher-training-entitlement-board/issues/225

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,35 @@
 class SessionsController < PublicPagesController
   def destroy
     admin = current_admin
+    user = current_user
+    id_token = session[:id_token]
 
     sign_out_all_scopes
 
-    if admin
-      redirect_to(root_path)
-    else
-      redirect_to build_sign_out_uri, allow_other_host: true
-    end
+    redirect_to("/admin") and return if admin
+    redirect_to(root_path) and return unless user
+
+    redirect_to build_sign_out_uri(id_token), allow_other_host: true
   end
 
 private
 
-  def build_sign_out_uri
+  def build_sign_out_uri(id_token)
     teacher_auth_domain_uri = URI.parse(ENV["TEACHER_AUTH_DOMAIN"])
 
     URI::Generic.build({
       scheme: teacher_auth_domain_uri.scheme,
       host: teacher_auth_domain_uri.host,
       port: teacher_auth_domain_uri.port,
-      path: "/connect/signout",
+      path: "/oauth2/logout",
       query: URI.encode_www_form({
-        "client_id" => ENV.fetch("TEACHER_AUTH_CLIENT_ID"),
-        "post_logout_redirect_uri" => root_uri.to_s,
+        "id_token_hint" => id_token,
+        "post_logout_redirect_uri" => post_logout_uri.to_s,
       }),
     }).to_s
   end
 
-  def root_uri
-    return "http://localhost:#{request.port}/" if request.host == "localhost"
-
-    URI::HTTPS.build(host: request.host, path: "/")
+  def post_logout_uri
+    URI.join(ENV.fetch("HOSTING_DOMAIN"), "/sign-out").to_s
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,14 +3,33 @@ require "rails_helper"
 RSpec.describe SessionsController do
   include Helpers::JourneyHelper
 
-  it "redirects to the external OIDC provider's signout endpoint" do
-    expect(controller).to receive(:sign_out_all_scopes)
+  context "when a user is signed in" do
+    before do
+      allow(controller).to receive(:current_user).and_return(create(:user))
+    end
 
-    root_path = "https://test.host/"
-    expected_redirect_url = "https://teacher-auth-domain.com:443/connect/signout?client_id=teacher-training-entitlement&post_logout_redirect_uri=#{CGI.escape(root_path)}"
+    it "redirects to the external OIDC provider's signout endpoint" do
+      expect(controller).to receive(:sign_out_all_scopes)
 
-    get :destroy
+      id_token = "test-id-token"
+      session[:id_token] = id_token
 
-    expect(response).to redirect_to(expected_redirect_url)
+      post_logout_uri = "http://test.host/sign-out"
+      expected_redirect_url = "https://teacher-auth-domain.com:443/oauth2/logout?id_token_hint=#{id_token}&post_logout_redirect_uri=#{CGI.escape(post_logout_uri)}"
+
+      get :destroy
+
+      expect(response).to redirect_to(expected_redirect_url)
+    end
+  end
+
+  context "when no user is signed in (callback from provider)" do
+    it "redirects to root" do
+      expect(controller).to receive(:sign_out_all_scopes)
+
+      get :destroy
+
+      expect(response).to redirect_to(root_path)
+    end
   end
 end
