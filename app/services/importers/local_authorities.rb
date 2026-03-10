a014238bf77e05b83f87d41c39ebef0665747cff
Toby Privett <toby@snaplab.co.uk>
2026-03-10 14:52:09 +0000

    Fix LocalAuthority import to write to Institution

--- app/services/importers/local_authorities.rb
+++ app/services/importers/local_authorities.rb
@@ -14,20 +14,33 @@ class Importers::LocalAuthorities
       la = LocalAuthority.find_or_initialize_by(ukprn: row["ukprn"])
 
       la.update!(
-        name: row["name"],
-        address_1: row["address_1"],
-        address_2: row["address_2"],
-        address_3: row["address_3"],
-        town: row["town"],
-        county: row["county"],
-        postcode: row["postcode"],
-        postcode_without_spaces: row["postcode"]&.gsub(" ", ""),
         high_pupil_premium: ActiveModel::Type::Boolean.new.cast(row["high_pupil_premium"]),
       )
+
+      update_institution!(la, row)
     end
   end
 
 private
+
+  def update_institution!(la, row)
+    institution_attrs = {
+      name: row["name"],
+      address_1: row["address_1"],
+      address_2: row["address_2"],
+      address_3: row["address_3"],
+      town: row["town"],
+      county: row["county"],
+      postcode: row["postcode"],
+      postcode_without_spaces: row["postcode"]&.gsub(" ", ""),
+    }
+
+    if la.institution
+      la.institution.update!(institution_attrs)
+    else
+      la.create_institution!(institution_attrs)
+    end
+  end
 
   def rows
     @rows ||= CSV.read(path_to_csv, headers: true)
