de15799a03102053cd921fb7c558f6c32229a86d
Toby Privett <toby@snaplab.co.uk>
2026-03-10 13:50:38 +0000

    Update seeds to create Institution records

--- db/seeds/base/add_childcare_providers.rb
+++ db/seeds/base/add_childcare_providers.rb
@@ -1,19 +1,33 @@
 CSV.read(Rails.root.join("db/seeds/data/private_childcare_providers.csv"), headers: true).tap do |data|
   import_count = 0
-  batch = []
 
   data.each do |row|
-    batch << row.to_h
-    next unless batch.length >= 1000
+    provider = PrivateChildcareProvider.find_or_initialize_by(id: row["id"])
+    provider.assign_attributes(
+      provider_urn: row["provider_urn"],
+      local_authority: row["local_authority"],
+      early_years_individual_registers: row["early_years_individual_registers"],
+      provider_early_years_register_flag: row["provider_early_years_register_flag"],
+      provider_compulsory_childcare_register_flag: row["provider_compulsory_childcare_register_flag"],
+      created_at: row["created_at"],
+      updated_at: row["updated_at"],
+    )
+    provider.save!
 
-    Rails.logger.info("Importing #{import_count += 1000} private childcare providers")
+    provider.institution || provider.create_institution!(
+      name: row["provider_name"],
+      address_1: row["address_1"],
+      address_2: row["address_2"],
+      address_3: row["address_3"],
+      town: row["town"],
+      postcode: row["postcode"],
+      postcode_without_spaces: row["postcode_without_spaces"],
+      region: row["region"],
+    )
 
-    PrivateChildcareProvider.insert_all(batch)
-    batch = []
+    import_count += 1
+    Rails.logger.info("Importing #{import_count} private childcare providers") if (import_count % 1000).zero?
   end
 
-  unless batch.empty?
-    Rails.logger.info("Importing #{import_count + batch.length} private childcare providers")
-    PrivateChildcareProvider.insert_all(batch)
-  end
+  Rails.logger.info("Importing #{import_count} private childcare providers")
 end
--- db/seeds/base/add_schools.rb
+++ db/seeds/base/add_schools.rb
@@ -1,19 +1,43 @@
 CSV.read(Rails.root.join("db/seeds/data/schools.csv"), headers: true).tap do |data|
   import_count = 0
-  batch = []
 
   data.each do |row|
-    batch << row.to_h
-    next unless batch.length >= 1000
+    school = School.find_or_initialize_by(urn: row["urn"])
+    school.assign_attributes(
+      la_code: row["la_code"],
+      la_name: row["la_name"],
+      close_date: row["close_date"],
+      ukprn: row["ukprn"],
+      last_changed_date: row["last_changed_date"],
+      establishment_type_code: row["establishment_type_code"],
+      establishment_type_name: row["establishment_type_name"],
+      establishment_status_code: row["establishment_status_code"],
+      establishment_status_name: row["establishment_status_name"],
+      high_pupil_premium: row["high_pupil_premium"],
+      number_of_pupils: row["number_of_pupils"],
+      eyl_funding_eligible: row["eyl_funding_eligible"],
+      phase_type: row["phase_type"],
+      phase_name: row["phase_name"],
+      created_at: row["created_at"],
+      updated_at: row["updated_at"],
+    )
+    school.save!
 
-    Rails.logger.info("Importing #{import_count += 1000} schools")
+    school.institution || school.create_institution!(
+      name: row["name"],
+      address_1: row["address_1"],
+      address_2: row["address_2"],
+      address_3: row["address_3"],
+      town: row["town"],
+      county: row["county"],
+      postcode: row["postcode"],
+      postcode_without_spaces: row["postcode_without_spaces"],
+      region: row["region"],
+    )
 
-    School.insert_all(batch)
-    batch = []
+    import_count += 1
+    Rails.logger.info("Importing #{import_count} schools") if (import_count % 1000).zero?
   end
 
-  unless batch.empty?
-    Rails.logger.info("Importing #{import_count + batch.length} schools")
-    School.insert_all(batch)
-  end
+  Rails.logger.info("Importing #{import_count} schools")
 end
