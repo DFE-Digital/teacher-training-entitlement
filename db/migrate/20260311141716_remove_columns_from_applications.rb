31d4e580b14ec87fe2f2dccab5472cddbe2175ee
Ben Keeping <benjaminkeeping@gmail.com>
2026-03-20 11:17:27 +0000

    fix migration

--- db/migrate/20260311141716_remove_columns_from_applications.rb
+++ db/migrate/20260311141716_remove_columns_from_applications.rb
@@ -16,7 +16,6 @@ class RemoveColumnsFromApplications < ActiveRecord::Migration[8.1]
       remove_column :applications, :teacher_catchment_synced_to_ecf, :boolean
       remove_column :applications, :DEPRECATED_cohort, :integer
       remove_column :applications, :DEPRECATED_itt_provider, :string
-      remove_column :applications, :DEPRECATED_private_childcare_provider_urn, :text
 
       drop_enum :headteacher_statuses
       drop_enum :employment_types
