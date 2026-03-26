4b5fba231396f1541509da481daa2562c982bec2
Toby Privett <toby@snaplab.co.uk>
2026-03-26 14:20:29 +0000

    Remove redundant code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def application_count_based_account_url
-    current_user.applications.size == 1 ? application_path(current_user.applications.first.ecf_id) : applications_path
-  end
-
   def npq_registration_link
     if signed_in?
       if Feature.trn_required? && current_user.trn.blank?
