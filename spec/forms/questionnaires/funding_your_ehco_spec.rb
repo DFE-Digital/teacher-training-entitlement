ad11ef568cf6ecf8b14c5822592a8b8b52f3f52a
Ben Keeping <benjaminkeeping@gmail.com>
2026-03-27 10:38:29 +0000

    Remove ehco code/references (#264)
    Remove EHCO references and code because we don't support it.

--- spec/forms/questionnaires/funding_your_ehco_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Questionnaires::FundingYourEhco, :npq, type: :model do
-  it { is_expected.to validate_inclusion_of(:ehco_funding_choice).in_array(Questionnaires::FundingYourEhco::VALID_FUNDING_OPTIONS) }
-end
