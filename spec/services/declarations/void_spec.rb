11bb7e140d340de870c104dbb8d65b3cee3f870c
Ben Keeping <benjaminkeeping@gmail.com>
2026-04-01 07:50:31 +0100

    clean up spec

--- spec/services/declarations/void_spec.rb
+++ spec/services/declarations/void_spec.rb
@@ -5,22 +5,22 @@ require "rails_helper"
 RSpec.describe Declarations::Void, type: :model do
   let(:application) { create(:application, :started) }
   let(:statement) { create(:statement, :next_output_fee) }
-  let(:declaration_type) { :started }
-  let(:declaration) { create(:declaration, declaration_type, application:, lead_provider: statement.lead_provider, cohort: statement.cohort) }
+  let(:declaration_trait) { :started }
+  let(:declaration) { create(:declaration, declaration_trait, application:, lead_provider: statement.lead_provider, cohort: statement.cohort) }
 
   subject(:service) { described_class.new(declaration:) }
 
   describe "validations" do
     context "when voiding the declaration" do
       context "when the application has been completed and the declaration is started" do
-        let(:declaration_type) { :started }
+        let(:declaration_trait) { :started }
         let(:application) { create(:application, :completed) }
 
         it { expect(service).to have_error(:base, :application_status_completed, I18n.t("declaration.application_status_completed")) }
       end
 
       context "when the application has been completed and the declaration is completed" do
-        let(:declaration_type) { :completed }
+        let(:declaration_trait) { :completed }
         let(:application) { create(:application, :completed) }
 
         it {
@@ -29,65 +29,53 @@ RSpec.describe Declarations::Void, type: :model do
         }
       end
 
-      Declaration::VOIDABLE_STATES.each do |state|
-        context "with a #{state} declaration" do
-          before { declaration.update!(state:) }
+      context "when the declaration is already voided" do
+        let(:declaration_state) { :voided }
 
-          context "when the declaration is already voided" do
-            before do
-              declaration.update!(state: :voided)
-              service.call
-            end
-
-            it { expect(service).to have_error(:base, :already_voided, "This declaration has already been voided.") }
-          end
-        end
+        it {
+          service.call
+          expect(service).to have_error(:base, :already_voided, "This declaration has already been voided.")
+        }
       end
     end
   end
 
-  describe "#void" do
-    subject(:void) { service.call }
+  Declaration::VOIDABLE_STATES.each do |declaration_state|
+    context "when voiding a #{declaration_state} declaration" do
+      let(:declaration_trait) { declaration_state }
 
-    it { is_expected.to be(true) }
+      it { expect { service.call }.to change { declaration.reload.state }.from(declaration_state).to("voided") }
 
-    Declaration::VOIDABLE_STATES.each do |declaration_state|
-      context "when voiding a #{declaration_state} declaration" do
-        before { declaration.update!(state: declaration_state) }
+      %w[eligible ineligible payable].each do |statement_item_state|
+        context "when the declaration has a #{statement_item_state} statement item" do
+          let(:statement_item) { create(:statement_item, declaration:, state: statement_item_state) }
 
-        it { expect { void }.to change { declaration.reload.state }.from(declaration_state).to("voided") }
-
-        %w[eligible ineligible payable].each do |statement_item_state|
-          context "when the declaration has a #{statement_item_state} statement item" do
-            let(:statement_item) { create(:statement_item, declaration:, state: statement_item_state) }
-
-            it { expect { void }.to change { statement_item.reload.state }.from(statement_item_state).to("voided") }
-          end
-        end
-
-        it "calls the void participant outcome service" do
-          service_double = instance_double(ParticipantOutcomes::Void)
-          allow(service_double).to receive(:void_outcome)
-          expect(service_double).to receive(:void_outcome)
-
-          allow(ParticipantOutcomes::Void).to receive(:new).with(declaration:).and_return(service_double)
-          expect(ParticipantOutcomes::Void).to receive(:new).with(declaration:)
-
-          void
+          it { expect { service.call }.to change { statement_item.reload.state }.from(statement_item_state).to("voided") }
         end
       end
-    end
 
-    context "when not valid" do
-      before { declaration.update!(state: :voided) }
+      it "calls the void participant outcome service" do
+        service_double = instance_double(ParticipantOutcomes::Void)
+        allow(service_double).to receive(:void_outcome)
+        expect(service_double).to receive(:void_outcome)
 
-      it { expect { void }.not_to(change { declaration.reload.state }) }
+        allow(ParticipantOutcomes::Void).to receive(:new).with(declaration:).and_return(service_double)
+        expect(ParticipantOutcomes::Void).to receive(:new).with(declaration:)
+
+        service.call
+      end
     end
+  end
+
+  context "when not valid" do
+    let(:declaration_trait) { :voided }
+
+    it { expect { service.call }.not_to(change { declaration.reload.state }) }
   end
 
   describe "Updating application status when voiding" do
     context "when voiding a started declaration" do
-      let(:declaration_type) { :started }
+      let(:declaration_trait) { :started }
 
       before do
         create(:application_state, :accepted, application:)
@@ -102,7 +90,7 @@ RSpec.describe Declarations::Void, type: :model do
     end
 
     context "when voiding a completed declaration" do
-      let(:declaration_type) { :completed }
+      let(:declaration_trait) { :completed }
 
       before do
         create(:application_state, :accepted, application:)
