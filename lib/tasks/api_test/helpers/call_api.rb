91b0108f18384989772822a1f7ac81b32672d708
Ben Keeping <benjaminkeeping@gmail.com>
2026-04-21 16:25:24 +0100

    Add rake task to call the resume application API endpoint (#329)
    ### Context
    
    Ticket:
    https://github.com/DFE-Digital/teacher-training-entitlement-board/issues/280
    
    ### Changes proposed in this pull request
    
    Added a rake task to test the resume application API endpoint locally

---
+++ lib/tasks/api_test/helpers/call_api.rb
@@ -0,0 +1,23 @@
+module CallApi
+  def call_api(lead_provider:, url:, body:)
+    api_token = generate_token!(lead_provider:)
+
+    headers = {
+      "Authorization" => "Bearer #{api_token}",
+      "Content-Type" => "application/json",
+    }
+
+    response = HTTParty.put(url, body:, headers:)
+
+    APIToken.find_by_unhashed_token(api_token, scope: :lead_provider).delete
+
+    response
+  end
+
+private
+
+  def generate_token!(lead_provider:)
+    scope = APIToken.scopes[:lead_provider]
+    APIToken.create_with_random_token!(scope:, lead_provider:)
+  end
+end
---
+++ lib/tasks/api_test/helpers/defer_application.rb
@@ -0,0 +1,38 @@
+require_relative "call_api"
+
+class DeferApplication
+  include CallApi
+  include Rails.application.routes.url_helpers
+
+  def initialize(application: nil)
+    @application = application
+  end
+
+  def call
+    if application.nil?
+      raise "[DeferApplication] Could not find a deferred application"
+    end
+
+    body = {
+      data: {
+        type: "application",
+        attributes: {
+          reason: "other",
+        },
+      },
+    }.to_json
+
+    url = defer_api_v1_application_url(application.ecf_id, host: "localhost:3000")
+
+    response = call_api(lead_provider: application.lead_provider, url:, body:)
+
+    puts "status: #{response.code}" # rubocop:disable Rails/Output
+    puts "response: #{response.parsed_response}" # rubocop:disable Rails/Output
+  end
+
+private
+
+  def application
+    @application ||= Application.started_status.last
+  end
+end
