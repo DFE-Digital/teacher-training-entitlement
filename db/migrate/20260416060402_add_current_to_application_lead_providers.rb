5a3001f83946435bcf6a8952c6de7bd8e90c94c9
Ben Keeping <benjaminkeeping@gmail.com>
2026-04-29 09:12:18 +0100

    fix migration

--- db/migrate/20260416060402_add_current_to_application_lead_providers.rb
+++ db/migrate/20260416060402_add_current_to_application_lead_providers.rb
@@ -1,4 +1,4 @@
-class AddActiveToApplicationLeadProviders < ActiveRecord::Migration[8.1]
+class AddCurrentToApplicationLeadProviders < ActiveRecord::Migration[8.1]
   def change
     add_column :application_lead_providers, :current, :boolean, default: false
   end
