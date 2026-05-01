76b0c487e043613a6181d32958cd7f1a78dae8c9
Ben Keeping <benjaminkeeping@gmail.com>
2026-05-01 11:29:48 +0100

    fix typo again

--- app/controllers/super_admin_controller.rb
+++ app/controllers/super_admin_controller.rb
@@ -5,7 +5,7 @@ private
 
   def require_super_admin
     unless current_admin.super_admin?
-      flash[:alert] = { title: "Unauthorized", message: "Sign in with your adminstrator account" }
+      flash[:alert] = { title: "Unauthorized", message: "Sign in with your administrator account" }
       redirect_to sign_in_path
     end
   end
