1652b7fb9e23e973b000c199ed79844540fdca8e
Ben Keeping <benjaminkeeping@gmail.com>
2026-05-06 12:06:38 +0100

    Fix issue on deferral also

--- spec/serializers/api/participant_serializer_spec.rb
+++ spec/serializers/api/participant_serializer_spec.rb
@@ -166,6 +166,27 @@ RSpec.describe API::ParticipantSerializer, type: :serializer do
             }.deep_stringify_keys,
           ])
         end
+
+        context "when withdrawn state change is missing lead provider" do
+          before { deferral.update_columns(lead_provider_id: nil) }
+
+          it do
+            expect(attributes["enrolments"]).not_to be_nil
+          end
+        end
+      end
+
+      context "when application has been accepted" do
+        let(:application) { create(:application, :with_declaration, :accepted, :with_accepted_event, :eligible_for_funded_place, lead_provider:) }
+        let(:accepted) { application.state_changes.where(event: Application::ACCEPTED).first }
+
+        context "when accepted state change is missing lead provider" do
+          before { accepted.update_columns(lead_provider_id: nil) }
+
+          it do
+            expect(attributes["enrolments"]).not_to be_nil
+          end
+        end
       end
 
       it "serializes the `participant_id_changes`" do
