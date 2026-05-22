bb8904278a62e9012e5414f4b63f437587d7bf91
Toby Privett <toby@snaplab.co.uk>
2026-05-22 11:07:05 +0100

    Replace Rails credentials with Azure Key Vault ENV vars

--- config/initializers/active_record_encryption.rb
+++ config/initializers/active_record_encryption.rb
@@ -1,9 +1,8 @@
 # Test environment encryption keys are set in config/environments/test.rb
-# to ensure they're available before eager loading.
 unless Rails.env.test?
   Rails.application.configure do
-    config.active_record.encryption.primary_key = Rails.application.credentials.dig(:active_record_encryption, :primary_key)
-    config.active_record.encryption.deterministic_key = Rails.application.credentials.dig(:active_record_encryption, :deterministic_key)
-    config.active_record.encryption.key_derivation_salt = Rails.application.credentials.dig(:active_record_encryption, :key_derivation_salt)
+    config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+    config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
   end
 end
