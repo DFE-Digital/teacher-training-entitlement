93350275cdd1dd5b39a055525c49b7f86c754c9d
Ben Keeping <benjaminkeeping@gmail.com>
2026-05-26 10:47:51 +0100

    Add feature specs for all variations of registration journey

---
+++ spec/features/journeys/reception_registration/private_setting_not_in_england_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+RSpec.feature "Registration wizard paths", :no_js, :with_default_lead_provider, :with_default_schedules, type: :feature do
+  include Helpers::JourneyAssertionHelper
+  include Helpers::ReceptionRegistrationPathHelper
+  include ApplicationHelper
+
+  include_context "Stub Teacher Auth Responses"
+
+  scenario "private setting not in England without funded place" do
+    lead_provider = LeadProvider.first
+
+    start_registration
+    choose_current_course_start_date
+    choose_provider(lead_provider)
+    choose_teacher_catchment("No")
+    choose_work_setting("Private nursery, pre-school or school")
+    continue_past_ineligible_funding
+    choose_course_funding("I am paying")
+    agree_to_share_provider_information
+    submit_check_answers
+
+    application = Application.sole
+
+    aggregate_failures "application fields" do
+      expect(application.course).to eq(Course.reception)
+      expect(application.lead_provider).to eq(lead_provider)
+      expect(application.institution).to be_nil
+      expect(application.user.email).to eq(user_email)
+
+      expect(application).to have_attributes(
+        eligible_for_funding: false,
+        funded_place: nil,
+        funding_choice: "self",
+        funding_eligiblity_status_code: "not_in_england",
+        institution_id: nil,
+        teacher_catchment: "another",
+        teacher_catchment_country: nil,
+        teacher_catchment_iso_country_code: nil,
+        targeted_support_funding_eligibility: false,
+        ukprn: nil,
+        work_setting: Institution::PRIVATE_INSTITUTION,
+      )
+
+      expect(application.raw_application_data).to match(
+        "can_share_choices" => "1",
+        "course_identifier" => Course.reception.identifier,
+        "course_start" => "In #{application_course_start_date}",
+        "course_start_date" => "yes",
+        "funding" => "self",
+        "funding_amount" => nil,
+        "lead_provider_id" => lead_provider.id.to_s,
+        "submitted" => true,
+        "teacher_catchment" => "another",
+        "teacher_catchment_country" => nil,
+        "work_setting" => Institution::PRIVATE_INSTITUTION,
+      )
+    end
+  end
+end
---
+++ spec/support/helpers/reception_registration_path_helper.rb
@@ -0,0 +1,53 @@
+module Helpers
+  module ReceptionRegistrationPathHelper
+    def start_registration
+      navigate_to_page(path: "/", submit_form: false) do
+        page.click_button("Start now")
+      end
+    end
+
+    def choose_current_course_start_date
+      expect_page_to_have(path: "/registration/course-start-date", submit_form: true) do
+        page.choose(application_course_start_date, visible: :all)
+      end
+    end
+
+    def choose_provider(lead_provider)
+      expect_page_to_have(path: "/registration/choose-your-provider", submit_form: true) do
+        page.choose(lead_provider.name, visible: :all)
+      end
+    end
+
+    def choose_teacher_catchment(answer)
+      expect_page_to_have(path: "/registration/teacher-catchment", submit_form: true) do
+        page.choose(answer, visible: :all)
+      end
+    end
+
+    def choose_work_setting(label)
+      expect_page_to_have(path: "/registration/work-setting", submit_form: true) do
+        page.choose(label, visible: :all)
+      end
+    end
+
+    def continue_past_ineligible_funding
+      expect_page_to_have(path: "/registration/ineligible-for-funding", click_continue: true)
+    end
+
+    def choose_course_funding(label)
+      expect_page_to_have(path: "/registration/funding-your-course", submit_form: true) do
+        page.choose(label, visible: :all)
+      end
+    end
+
+    def agree_to_share_provider_information
+      expect_page_to_have(path: "/registration/share-provider", submit_form: true) do
+        page.check("Yes, I agree to share my information", visible: :all)
+      end
+    end
+
+    def submit_check_answers
+      expect_page_to_have(path: "/registration/check-answers", submit_button_text: "Submit", submit_form: true)
+    end
+  end
+end
