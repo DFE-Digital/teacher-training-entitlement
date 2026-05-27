c410897857c51da27a7e2c514a130f6926cf6434
Jerome Riga <jriga@zemis.co.uk>
2026-05-27 18:14:45 +0100

    Create LP account for security test
    Fixes DFE-Digital/teacher-training-entitlement-board#435

--- db/seeds/base/constants.rb
+++ db/seeds/base/constants.rb
@@ -4,34 +4,14 @@ LEAD_PROVIDER_CONFIG = {
     url: "https://www.ambition.org.uk/",
     hint: "Ambition Institute is a graduate school with programmes designed to support educators throughout the education sector, including teachers, leaders, and executive leaders.",
   },
-  "Best Practice Network" => {
-    token: "best-practice-token",
-    url: "https://www.bestpracticenet.co.uk/",
-    hint: "We share the desire of every practitioner that every child, regardless of their background, should benefit from an excellent education.",
-  },
-  "Church of England" => {
-    token: "coe-token",
-    url: "https://www.nse.org.uk/",
-    hint: "TBD",
-  },
-  "University College London (UCL) Institute of Education" => {
-    token: "ucl-token",
-    url: "https://www.ucl.ac.uk/ioe/courses/professional-development-teachers/",
-    hint: "We work across education, culture, psychology and social science to create lasting and evolving change.",
-  },
-  "National Institute of Teaching" => {
-    token: "niot-token",
-    url: "https://niot.org.uk/",
-    hint: "We research and run professional development for teachers and school leaders for the benefit of pupils across England. #traintoteach",
-  },
-  "LLSE" => {
-    token: "llse-token",
-    url: "https://www.llse.org.uk/",
-    hint: "An Outstanding Lead Provider of National Professional Qualifications (NPQs) for School Leadership",
-  },
   "Teach First" => {
     token: "teach-first-token",
     url: "https://teachfirst.co.uk",
     hint: "Teach first hint",
+  },
+  "forfendinfosec" => {
+    token: "forfendinfosec-token",
+    url: "forfendinfosec.com",
+    hint: "forfendinfosec",
   },
 }.freeze
