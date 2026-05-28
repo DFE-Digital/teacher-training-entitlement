b014a88bb9eddc70c8e7dd601171d5d7caf1000d
Jerome Riga <jriga@zemis.co.uk>
2026-05-28 13:38:54 +0100

    Add super admin

--- db/seeds/base/add_users.rb
+++ db/seeds/base/add_users.rb
@@ -8,3 +8,14 @@ AdminUser.find_or_create_by!(email: "bianca.napoleonov@education.gov.uk") do |ad
     super_admin: false,
   )
 end
+
+# Create super admin user
+AdminUser.find_or_create_by!(email: "superadmin@example.com") do |admin|
+  admin.assign_attributes(
+    full_name: "example super admin",
+    otp_hash: otp_testing_code,
+    otp_expires_at: "3000-01-01 00:00:00.000000000 +0000",
+    super_admin:
+    true,
+  )
+end
