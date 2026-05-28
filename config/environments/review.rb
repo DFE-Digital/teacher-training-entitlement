42fc39b54c28c07d563a7e0b811d33130516c972
Jerome Riga <jriga@zemis.co.uk>
2026-05-28 11:03:28 +0100

    Disable bullet on review / dev env
    created a ticket 455 to capture current issue

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable                       = true
     Bullet.bullet_logger                = false
-    Bullet.raise                        = true # Raise an error if n+1 query occurs
+    # this is for error in admin finance statements assurance query
+    Bullet.raise                        = false # Raise an error if n+1 query occurs
     Bullet.unused_eager_loading_enable  = false # Disabled due to the way our queries are structured
   end
 end
