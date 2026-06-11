8220ea521e83b41f949e8f917f7edb3723a97b48
Ben Keeping <benjaminkeeping@gmail.com>
2026-06-11 14:09:25 +0100

    Remove unused registration code (#502)
    Remove code relating to steps which have been removed from registration

--- spec/controllers/registration_wizard_controller_spec.rb
+++ spec/controllers/registration_wizard_controller_spec.rb
@@ -33,13 +33,13 @@ RSpec.describe RegistrationWizardController do
         { "works_in_childcare" => "yes", "kind_of_nursery" => "private_nursery" }
       end
 
-      it { is_expected.to redirect_to registration_wizard_show_path("have-ofsted-urn") }
+      it { is_expected.to redirect_to registration_wizard_show_path("work-setting") }
     end
 
     context "when working in an early years setting" do
       let(:registration_store) { { "works_in_childcare" => "yes" } }
 
-      it { is_expected.to redirect_to registration_wizard_show_path("choose-childcare-provider") }
+      it { is_expected.to redirect_to registration_wizard_show_path("work-setting") }
     end
   end
 
