2d8c59606118b79f4327439602d85919369a0949
Ben Keeping <benjaminkeeping@gmail.com>
2026-06-16 16:56:00 +0100

    Don't hide previous providers in change provider flow

--- app/controllers/applications/change_provider/providers_controller.rb
+++ app/controllers/applications/change_provider/providers_controller.rb
@@ -27,7 +27,6 @@ module Applications
           .for(course: application.course)
           .alphabetical
           .reject { |p| p.id == application.lead_provider.id }
-          .reject { |p| p.id.in?(application.application_lead_providers.map(&:lead_provider_id)) }
       end
 
       def set_form
