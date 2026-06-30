52c806ab08742a997098a057dff12612c6f623e8
Ben Keeping <benjaminkeeping@gmail.com>
2026-06-30 11:24:20 +0100

    Update provider URLs

--- db/seeds/base/constants.rb
+++ db/seeds/base/constants.rb
@@ -1,32 +1,32 @@
 LEAD_PROVIDER_CONFIG = {
   "Ambition Institute" => {
     token: "ambition-token",
-    url: "https://www.ambition.org.uk/",
+    url: "https://www.ambition.org.uk/programmes/excellence-in-reception-teaching",
     hint: nil,
   },
   "Best Practice Network" => {
     token: "best-practice-token",
-    url: "https://www.bestpracticenet.co.uk/",
+    url: "https://www.bestpracticenet.co.uk/excellence-in-reception-teaching",
     hint: nil,
   },
   "Church of England" => {
     token: "coe-token",
-    url: "https://www.nse.org.uk/",
+    url: "https://www.nse.org.uk",
     hint: nil,
   },
   "University College London (UCL) Institute of Education" => {
     token: "ucl-token",
-    url: "https://www.ucl.ac.uk/ioe/courses/professional-development-teachers/",
+    url: "https://www.ucl.ac.uk/ioe/departments-and-centres/ucl-centre-educational-leadership/courses/excellence-reception-teaching-programme",
     hint: nil,
   },
   "National Institute of Teaching" => {
     token: "niot-token",
-    url: "https://niot.org.uk/",
+    url: "https://niot.org.uk/programmes/Reception",
     hint: nil,
   },
   "LLSE" => {
     token: "llse-token",
-    url: "https://www.llse.org.uk/",
+    url: "https://llse.org.uk/npd-rct",
     hint: nil,
   },
   "Teach First" => {
