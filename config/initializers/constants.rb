cfccd5727f04d107a95638c36794c6e5c9db82d9
Ben Keeping <benjaminkeeping@gmail.com>
2026-07-16 16:32:29 +0100

    Add anchor link to privacy policy url

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
 APRIL_2024_CUTOFF_DATE = Time.zone.local(2024, 4, 2)
 FEEDBACK_FORM_URL = "https://forms.cloud.microsoft/e/LfumWfMCY1".freeze
-PRIVACY_NOTICE_URL = "https://www.gov.uk/government/publications/privacy-information-education-providers-workforce-including-teachers/privacy-information-education-providers-workforce-including-teachers".freeze
+PRIVACY_NOTICE_URL = "https://www.gov.uk/government/publications/privacy-information-education-providers-workforce-including-teachers/privacy-information-education-providers-workforce-including-teachers#register-NPQ".freeze
