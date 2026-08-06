4ce73c7431a252578539d849967d43f0dfeea8cc
Jerome Riga <jriga@zemis.co.uk>
2026-08-06 14:17:33 +0100

    renamed custom_data_seeder.start_year to academic_year

--- app/jobs/custom_data_seeder_job.rb
+++ app/jobs/custom_data_seeder_job.rb
@@ -1,8 +1,8 @@
 class CustomDataSeederJob < ApplicationJob
   queue_as :default
 
-  def perform(lead_provider:, start_year:, nb_cohort:, nb_app_per_state:)
-    seeder = ValidTestDataGenerators::APITestScenariosSeeder.new(lead_provider:, academic_year: start_year)
+  def perform(lead_provider:, academic_year:, nb_cohort:, nb_app_per_state:)
+    seeder = ValidTestDataGenerators::APITestScenariosSeeder.new(lead_provider:, academic_year:)
 
     seeder.custom_data(nb_cohort:, nb_app_per_state:)
   end
