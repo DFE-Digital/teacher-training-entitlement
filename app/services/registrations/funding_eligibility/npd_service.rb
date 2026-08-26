c329d1b43bc993718ca6ab0f5d6a63b16bdb6bbc
Ben Keeping <benjaminkeeping@gmail.com>
2026-08-26 15:23:03 +0100

    fix

--- app/services/registrations/funding_eligibility/npd_service.rb
+++ app/services/registrations/funding_eligibility/npd_service.rb
@@ -8,10 +8,12 @@ module Registrations
         if wizard.state_store["teacher_catchment"] != "england"
           funding_eligibility_result_text = "You're not eligible because you're not in England"
           funding_eligibility_result = "ineligible"
+        elsif wizard.state_store["institution_id"].blank?
+          funding_eligibility_result_text = "You're not eligible because you're not a state setting"
+          funding_eligibility_result = "ineligible"
         else
           institution = Institution.find(wizard.state_store["institution_id"])
           if institution.eligible_establishment?
-
             funding_eligibility_result_text = "You're eligible for funding"
             funding_eligibility_result = "funded"
           else
