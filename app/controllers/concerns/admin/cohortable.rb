1518c4ed6bd1ddbdb1539b484c525e18fbb737d1
Jerome Riga <jriga@zemis.co.uk>
2026-09-03 22:32:07 +0100

    redirect to academic-year on naked index request

--- app/controllers/concerns/admin/cohortable.rb
+++ app/controllers/concerns/admin/cohortable.rb
@@ -4,6 +4,7 @@ module Admin
 
     included do
       before_action :set_cohorts
+      before_action :redirect_to_current_academic_year, only: [:index]
     end
 
   protected
@@ -15,6 +16,12 @@ module Admin
                                      .order("course_cohorts.cohort_id, cohorts.registration_starts_at DESC")
       @current_cohort = params[:cohort_id] ? Cohort.find(params[:cohort_id]) : nil
       @current_academic_year = params[:academic_year].presence&.to_i
+    end
+
+    def redirect_to_current_academic_year
+      if params[:cohort_id].blank? && params[:academic_year].blank?
+        redirect_to request.path + "/academic-years/#{Date.current.year}"
+      end
     end
   end
 end
