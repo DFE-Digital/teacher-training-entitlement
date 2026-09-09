fa115ecbd6aa02e0a80e14f7242874714a449fb3
Ben Keeping <benjaminkeeping@gmail.com>
2026-09-09 10:32:32 +0100

    fix

--- spec/requests/api/docs/v1/participants_spec.rb
+++ spec/requests/api/docs/v1/participants_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe "Participants endpoint", openapi_spec: "v1/swagger.yaml", type: :
   end
   let!(:participant) { application.user }
 
+  before do
+    application.course_cohort.update!(
+      schedule: create(:schedule, :tte_reception_autumn, cohort: application.cohort),
+    )
+  end
+
   it_behaves_like "an API index endpoint documentation",
                   "/api/v1/participants",
                   "Participants",
