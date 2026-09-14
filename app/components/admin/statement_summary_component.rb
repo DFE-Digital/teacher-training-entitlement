2fb3acea796e1357a3a34c304c0b55ba356b8e7a
Jerome Riga <jriga@zemis.co.uk>
2026-09-14 20:00:04 +0100

    Add details links to statement summary
    - received
    - expected
    - outstanding
    
    Fixes DFE-Digital/teacher-training-entitlement-board#XXX

--- app/components/admin/statement_summary_component.rb
+++ app/components/admin/statement_summary_component.rb
@@ -3,6 +3,7 @@
 module Admin
   class StatementSummaryComponent < BaseComponent
     include StatementHelper
+    include Rails.application.routes.url_helpers
 
     attr_reader :calculator, :link_to_voids, :statement
 
@@ -21,7 +22,35 @@ module Admin
       @statement = statement
     end
 
+    def outstanding_link(row)
+      link_to(
+        row[:outstanding],
+        outstanding_admin_finance_statement_path(statement, milestone: milestone(row)),
+        class: "x-govuk-sub-navigation__link",
+      )
+    end
+
+    def expected_link(row)
+      link_to(
+        row[:expected],
+        expected_admin_finance_statement_path(statement, milestone: milestone(row)),
+        class: "x-govuk-sub-navigation__link",
+      )
+    end
+
+    def received_link(row)
+      link_to(
+        row[:received],
+        received_admin_finance_statement_path(statement, milestone: milestone(row), funded: "yes"),
+        class: "x-govuk-sub-navigation__link",
+      )
+    end
+
   private
+
+    def milestone(row)
+      row[:declaration_type] unless row[:declaration_type].downcase == "total"
+    end
 
     def label_with_hint(label, hint)
       tag.span(label) + tag.br + tag.span(hint, class: "govuk-hint govuk-!-font-size-16 govuk-!-margin-bottom-0")
